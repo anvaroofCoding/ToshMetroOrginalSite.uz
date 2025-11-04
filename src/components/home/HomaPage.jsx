de7a25c89f8fcf3a63a05227c3ba18665663b075
islom upravleniya <islomanvarov05@gmail.com>
2025-11-04 16:00:26 +0500

    mains

--- src/components/home/HomaPage.jsx
+++ src/components/home/HomaPage.jsx
@@ -21,12 +21,17 @@ export default function HomePage() {
         }}
         className="z-50 flex flex-col justify-center items-center"
       >
-        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-blue-50 to-blue-400 py-4">
           O'zbekiston Respublikasi Toshkent shahar <br /> Metropoliten DUK
         </motion.p>
-        <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-          <span>Join now →</span>
-          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+        <button
+          className="px-4 py-2 backdrop-blur-sm border bg-blue-300/10 border-blue-500/20 text-white mx-auto text-center rounded-full relative mt-4"
+          onClick={() => {
+            window.location.href = "/metro-tarixi";
+          }}
+        >
+          <span>Metropliten haqida →</span>
+          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-blue-500 to-transparent" />
         </button>
       </motion.div>
     </ImagesSlider>
